3024bc21fec8210c9a6249a82976853b1c6d3f73
feli619 <feli619@outlook.com>
2014-07-08 08:23:08 -0500

    Suma por teclado sum2.c

---
+++ sum2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>					
+int main(){						
+	int a,b,c;
+	printf("valor de a: ");
+	scanf("%d",&a);
+	printf("valor de b: ");
+	scanf("%d",&b);       
+	c = a + b;
+	printf("la suma es %d \n", c);
+}
+
+
+//git config --global user.name "feli619"
+  //  git config --global user.email feli619@outlook.com
